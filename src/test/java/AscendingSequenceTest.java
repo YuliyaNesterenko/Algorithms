96540a391a1ab1ab5dc0b0359ed5c4a6454435ce
Yuliya Nesterenko <kosmoviolin@gmail.com>
2022-02-05 11:57:51 +0300

    add AsendSeqensceTest

--- src/test/java/AscendingSequenceTest.java
+++ src/test/java/AscendingSequenceTest.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class AscendingSequenceTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AscendingSequenceTest {
+    @Test
+    public void TestAscendingSequence(){
+        int a = 10;
+        int b = 25;
+        int n = 5;
+        int[] expectedResult = {10, 15, 20, 25};
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        int[] actualResult = ascendingSequence.getAscendingSequence(a,b,n);
+
+        Assert.assertArrayEquals(expectedResult,
+                actualResult);
+    }
+    public void TestAscendingSequence1(){
+        int a = 10;
+        int b = -25;
+        int n = -5;
+        int[] expectedResult = {-1};
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        int[] actualResult = ascendingSequence.getAscendingSequence(a,b,n);
+
+        Assert.assertArrayEquals(expectedResult,
+                actualResult);
+    }
+    public void TestAscendingSequence2(){
+        int a = 10;
+        int b = 10;
+        int n = 2;
+        int[] expectedResult = {-1};
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        int[] actualResult = ascendingSequence.getAscendingSequence(a,b,n);
+
+        Assert.assertArrayEquals(expectedResult,
+                actualResult);
+    }
 }
